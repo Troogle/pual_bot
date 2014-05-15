abb5afaadf0441700b273b5aecc015dfc67c7e69
Troogle <sqh1994@163.com>
2014-05-16 02:21:55 +0800

    -0-

---
+++ plugins/fudu.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+#   Author  :   cold
+#   E-mail  :   wh_linux@126.com
+#   Date    :   14/01/16 12:13:09
+#   Desc    :   Õ³Ìù´úÂë²å¼ş
+#
+from plugins import BasePlugin
+
+class PastePlugin(BasePlugin):
+    def is_match(self, from_uin, content, type):
+        if type=='g':
+            if self.cont==content:
+                return True
+            self.cont=content
+        return False
+
+    def send(self, content, callback):
+        """ Ìù´úÂë """
+        callback(content)
+        self.cont=""
+
+    def handle_message(self, callback):
+        self.send(cont, callback)
